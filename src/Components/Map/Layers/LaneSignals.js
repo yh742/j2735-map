c91183042454e90bc7b8ed35306f7117a0e15696
Sean Hsu <seanhsu@Seans-MacBook-Pro.local>
2020-04-13 10:21:55 -0700

    added layers for signaling

---
+++ src/Components/Map/Layers/LaneSignals.js
@@ -0,0 +1,107 @@
+export const GreenSignal = {
+    "id": "state-main-green",
+    "type": "symbol",
+    "metadata": {"mapbox:group": "78bc5266126203977554590806fdcf17"},
+    "source": "composite",
+    "source-layer": "state-main-0n4jew",
+    "minzoom": 18,
+    "filter": ["any"],
+    "layout": {
+        "icon-image": "green-arrow",
+        "icon-size": [
+            "interpolate",
+            ["linear"],
+            ["zoom"],
+            0,
+            0,
+            18,
+            0.6,
+            22,
+            1.2
+        ],
+        "symbol-placement": "line",
+        "icon-rotate": -270,
+        "symbol-spacing": [
+            "interpolate",
+            ["linear"],
+            ["zoom"],
+            0,
+            30,
+            22,
+            60
+        ]
+    },
+    "paint": {}
+};
+
+export const YellowSignal = {
+    "id": "state-main-yellow",
+    "type": "symbol",
+    "metadata": {"mapbox:group": "78bc5266126203977554590806fdcf17"},
+    "source": "composite",
+    "source-layer": "state-main-0n4jew",
+    "minzoom": 18,
+    "filter": ["any"],
+    "layout": {
+        "icon-image": "yellow-arrow",
+        "icon-size": [
+            "interpolate",
+            ["linear"],
+            ["zoom"],
+            0,
+            0,
+            18,
+            0.6,
+            22,
+            1.2
+        ],
+        "symbol-placement": "line",
+        "icon-rotate": -270,
+        "symbol-spacing": [
+            "interpolate",
+            ["linear"],
+            ["zoom"],
+            0,
+            30,
+            22,
+            60
+        ]
+    },
+    "paint": {}
+};
+
+export const RedSignal = {
+    "id": "state-main-red",
+    "type": "symbol",
+    "metadata": {"mapbox:group": "78bc5266126203977554590806fdcf17"},
+    "source": "composite",
+    "source-layer": "state-main-0n4jew",
+    "minzoom": 18,
+    "filter": ["any"],
+    "layout": {
+        "icon-image": "red-arrow",
+        "icon-size": [
+            "interpolate",
+            ["linear"],
+            ["zoom"],
+            0,
+            0,
+            18,
+            0.6,
+            22,
+            1.2
+        ],
+        "symbol-placement": "line",
+        "icon-rotate": -270,
+        "symbol-spacing": [
+            "interpolate",
+            ["linear"],
+            ["zoom"],
+            0,
+            30,
+            22,
+            60
+        ]
+    },
+    "paint": {}
+};
---
+++ src/Components/Map/Layers/LaneTrajectories.js
@@ -0,0 +1,116 @@
+export const GreenTrajectory = {
+    "id": "state-main-traj-green",
+    "type": "symbol",
+    "metadata": {"mapbox:group": "3493ee614896046c26b77953c80175b9"},
+    "source": "composite",
+    "source-layer": "state-main-traj",
+    "minzoom": 18,
+    "filter": ["any"],
+    "layout": {
+        "icon-image": "green-arrow",
+        "icon-size": [
+            "interpolate",
+            ["linear"],
+            ["zoom"],
+            0,
+            0,
+            18,
+            0.6,
+            22,
+            1.2
+        ],
+        "icon-ignore-placement": true,
+        "icon-allow-overlap": true,
+        "symbol-placement": "line",
+        "icon-padding": 1,
+        "icon-rotate": 90,
+        "symbol-spacing": [
+            "interpolate",
+            ["linear"],
+            ["zoom"],
+            18,
+            20,
+            22,
+            40
+        ]
+    },
+    "paint": {}
+};
+
+export const YellowTrajectory = {
+    "id": "state-main-traj-yellow",
+    "type": "symbol",
+    "metadata": {"mapbox:group": "3493ee614896046c26b77953c80175b9"},
+    "source": "composite",
+    "source-layer": "state-main-traj",
+    "minzoom": 18,
+    "filter": ["any"],
+    "layout": {
+        "icon-image": "yellow-arrow",
+        "icon-size": [
+            "interpolate",
+            ["linear"],
+            ["zoom"],
+            0,
+            0,
+            18,
+            0.6,
+            22,
+            1.2
+        ],
+        "icon-ignore-placement": true,
+        "icon-allow-overlap": true,
+        "symbol-placement": "line",
+        "icon-padding": 1,
+        "icon-rotate": 90,
+        "symbol-spacing": [
+            "interpolate",
+            ["linear"],
+            ["zoom"],
+            18,
+            20,
+            22,
+            40
+        ]
+    },
+    "paint": {}
+};
+
+export const RedTrajectory = {
+    "id": "state-main-traj-red",
+    "type": "symbol",
+    "metadata": {"mapbox:group": "3493ee614896046c26b77953c80175b9"},
+    "source": "composite",
+    "source-layer": "state-main-traj",
+    "minzoom": 18,
+    "filter": ["any"],
+    "layout": {
+        "icon-image": "red-arrow",
+        "icon-size": [
+            "interpolate",
+            ["linear"],
+            ["zoom"],
+            0,
+            0,
+            18,
+            0.6,
+            22,
+            1.2
+        ],
+        "icon-ignore-placement": true,
+        "icon-allow-overlap": true,
+        "symbol-placement": "line",
+        "icon-padding": 1,
+        "icon-rotate": 90,
+        "symbol-spacing": [
+            "interpolate",
+            ["linear"],
+            ["zoom"],
+            18,
+            20,
+            22,
+            40
+        ]
+    },
+    "paint": {}
+};
